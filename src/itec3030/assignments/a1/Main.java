89388aa7b8cea4ada063758a5fbb5e4d40cbde41
Ali Malik <ialimalik99@gmail.com>
2021-02-01 10:55:06 -0500

    Add the packages

---
+++ src/itec3030/assignments/a1/Main.java
@@ -0,0 +1,18 @@
+package itec3030.assignments.a1;
+
+import itec3030.smarthome.standards.Furnace;
+import itec3030.smarthome.standards.NumericMeasurementInstrument;
+import itec3030.smarthome.standards.TemperatureSensor;
+
+import itec3030.assignments.a1.sensors.omnitemp.OmniTempSensorXS3;
+//import itec3030.assignments.a1.sensors.thermoset.ThermoSetX19.ThermosetX19;
+import itec3030.assignments.a1.actuators.saharah.SaharaFurnaceHL42;
+import itec3030.smarthome.standards.TemperatureSensor;
+
+import itec3030.assignments.a1.sensors.omnitemp.OmniTempSensorXS3;
+//import itec3030.assignments.a1.sensors.thermoset.ThermoSetX19.ThermosetX19;
+import itec3030.assignments.a1.actuators.saharah.SaharaFurnaceHL42;
+
+public class Main {
+
+}
